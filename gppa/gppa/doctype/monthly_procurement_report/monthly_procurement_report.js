c3c7b4694a71c3bdae0777a48a8aad12e8e10107
Momodou Khan <khanmomodou101@gmail,com>
2024-09-29 00:54:12 +0000

    first commit

---
+++ gppa/gppa/doctype/monthly_procurement_report/monthly_procurement_report.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2024, khan and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on("Monthly Procurement Report", {
+	refresh(frm) {
+    // ceate a custom 
+
+	},
+});
